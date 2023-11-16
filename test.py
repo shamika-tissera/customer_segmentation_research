49a5ec07720329a554af5a09a91a3f17d0bec5e9
Shamika Tissera <nimeshshamika@gmail.com>
2023-11-16 10:28:17 +0530

    working on comparison

--- test.py
+++ test.py
@@ -1,30 +1,13 @@
-from transformers import AutoTokenizer, AutoModel
-import torch
+import spacy
+nlp = spacy.load("en_core_web_lg")
+#nlp = spacy.load("en_core_web_md")
 
 
-#Mean Pooling - Take attention mask into account for correct averaging
-def mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+doc1 = nlp(u'the person wear red T-shirt')
+doc2 = nlp(u'this person is walking')
+doc3 = nlp(u'the boy wear red T-shirt')
 
 
-# Sentences we want sentence embeddings for
-sentences = ['This is an example sentence', 'Each sentence is converted']
-
-# Load model from HuggingFace Hub
-tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/paraphrase-MiniLM-L6-v2')
-model = AutoModel.from_pretrained('sentence-transformers/paraphrase-MiniLM-L6-v2')
-
-# Tokenize sentences
-encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
-
-# Compute token embeddings
-with torch.no_grad():
-    model_output = model(**encoded_input)
-
-# Perform pooling. In this case, max pooling.
-sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-
-print("Sentence embeddings:")
-print(sentence_embeddings)
+print(doc1.similarity(doc2)) 
+print(doc1.similarity(doc3))
+print(doc2.similarity(doc3))
